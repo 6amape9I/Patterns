21cba4cb72054b6cca83f5d011fae679803d0c6f
Тимур <167026983+6amape9I@users.noreply.github.com>
2024-12-23 05:16:02 +0300

    feat: DBConfig

---
+++ src/Core_and_Models/DB/DBConfig.kt
@@ -0,0 +1,46 @@
+package Core_and_Models.DB
+
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.util.LinkedList
+
+class DBConfig {
+    private var conn: Connection? = null
+
+    fun createConnection(){
+        val url = "jdbc:postgresql://localhost:5432/StudentsDB"
+        val user = "postgres"
+        val password = "Tima2706"
+        this.conn = null
+        try {
+            this.conn = DriverManager.getConnection(url, user, password)
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    fun getConnection() = this.conn;
+
+    fun executeSqlSelect(query:String): ResultSet? {
+        val res:LinkedList<HashMap<String,Any>>;
+        try {
+            val conn = getConnection()
+            val statement = conn?.createStatement()
+            if (statement != null) {
+                return statement.executeQuery(query)
+            }
+        } catch (e: java.lang.Exception) {
+            println(e.message)
+        }
+        return null;
+    }
+    fun executeSql(query:String){
+        try {
+            val conn = getConnection()
+            val affectedRows = conn?.prepareStatement(query)?.executeUpdate()
+        } catch (e: java.lang.Exception) {
+            println(e)
+        }
+    }
+}
